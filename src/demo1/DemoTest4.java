5c18965f510f5cd839dc47d70bc6230284a55fbb
karla <karla@192.168.1.154>
2025-01-29 20:07:09 -0600

    Add comment

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -2,7 +2,12 @@ package demo1;
 
 public class DemoTest4 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("JavaIsFun");
+		
+}
+	//add variables
+	public void add(int c, int d) {
+		System.out.println(c + d);
 		
 }
 }
